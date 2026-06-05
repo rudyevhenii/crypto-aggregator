eb7cd42c57e1c4c362854c1252c01543019f9fee
Yevhenii Rud <zhenniarud@gmail.com>
2026-06-05 17:04:09 +0300

    Initial commit

---
+++ src/main/java/dev/rudyevhenii/crypto_aggregator/CryptoAggregatorApplication.java
@@ -0,0 +1,13 @@
+package dev.rudyevhenii.crypto_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rudyevhenii/crypto_aggregator/CryptoAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rudyevhenii.crypto_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
